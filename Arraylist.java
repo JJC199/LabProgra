8dd7213dc6409a898262c0b7204a489ab71e9ddb
JJC199 <juancarranza96@hotmail.com>
2016-01-22 08:44:02 -0600

    creacion

---
+++ Arraylist.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LabProgra2.LabProgra;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Juan Carranza
+ */
+public class Arraylist {
+    ArrayList a = new ArrayList<>();
+}
---
+++ TestCalendar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LabProgra2.LabProgra;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Juan Carranza
+ */
+public class TestCalendar {
+   Calendar c = Calendar.getInstance();
+    public static void main(String[] args) {
+        
+    }
+}
